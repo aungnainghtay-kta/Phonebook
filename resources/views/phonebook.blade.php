7ca36583dc52994781f4bb7315b8e6bcffae8eff
Aung Naing Htay <aungnainghtay.main@gmail.com>
2019-02-01 16:18:14 +0630

    Initial commit

---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Phone book</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div id="app">
+	<myheader></myheader>
+	<myfooter></myfooter>
+</div>
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
